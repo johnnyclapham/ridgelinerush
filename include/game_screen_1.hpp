fe2bde929eef0b0447b3cec63d3be1381cee32f7
Johnny Clapham <71145206+johnnyclapham@users.noreply.github.com>
2021-03-29 21:10:02 -0400

    fixed playerview.updateView
    was missing the deltaMS*GAME_TIME_FACTOR parameter

--- include/game_screen_1.hpp
+++ include/game_screen_1.hpp
@@ -2,10 +2,10 @@
 #define GAMESCREEN1_HPP_INCLUDED
 
 #include <iostream>
-#include "cScreen.hpp"
 #include "Constants.h"
 #include "Logic.h"
 #include "PlayerView.h"
+#include "cScreen.hpp"
 
 class game_screen_1 : public cScreen
 {
@@ -40,22 +40,16 @@ int game_screen_1::Run (sf::RenderWindow &App)
     while(App.pollEvent(Event))
     {
       // Exit
-      if(Event.type == sf::Event::Closed){
+      if(Event.type == sf::Event::Closed)
         App.close();
+
+      if (Event.key.code == sf::Keyboard::Key::Escape){
+        std::cout << "title_screen_0 <- game_screen_1\n";
+        //return 0 calls the 0 position screen
+        //0 position screen is title_screen_0
+        return(0);
+
       }
-
-        // key pressed event
-        if (Event.type == sf::Event::KeyPressed)
-        {
-
-					if (Event.key.code == sf::Keyboard::Key::Escape){
-						std::cout << "title_screen_0 <- game_screen_1\n";
-            //return 0 calls the 0 position screen
-            //0 position screen is title_screen_0
-						return(0);
-
-					}
-				}
     }
 
     // TODO set up gameTimeFactor for adaptive frame rate
@@ -65,14 +59,15 @@ int game_screen_1::Run (sf::RenderWindow &App)
     // clear screen and fill with blue
     App.clear(sf::Color::Blue);
 
-    playerView.updateView();
+    playerView.updateView(deltaMS*GAME_TIME_FACTOR);
 
     // display
     App.display();
   }
-
-    //exit (should not be needed)
-    return (-1);
+  //exit (should not be needed)
+  return (-1);
 }
+
+
 
 #endif // GAMESCREEN1_HPP_INCLUDED
