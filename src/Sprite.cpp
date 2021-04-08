a6e35cc86c9d8e0653449257478be2751dce850c
Johnny Clapham <71145206+johnnyclapham@users.noreply.github.com>
2021-04-07 22:54:56 -0400

    #23 Sprite should now load on all machines
    added an error condition to change pathname of texture if addressing fails. This is for functionality on all platforms

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -20,9 +20,17 @@ void Sprite::update(float time){
 
 void Sprite::draw(sf::Vector2<float> position, sf::RenderWindow *window) {
     sf::Texture newTexture;
+    
     if(!newTexture.loadFromFile(texture)){
        //error
+       // new handling: if sprite fails to load, try alternate
+       // addressing method (for Mac OS)
+       std::cout << "failed to load: "<<texture<<" \n";
+       texture = "../"+texture;
+       std::cout << "now loading   : "<<texture<<" \n";
+       newTexture.loadFromFile(texture);
     }
+    
     sprite.setTexture(newTexture);
     sprite.setPosition(position);
     sprite.setScale(4,4);
