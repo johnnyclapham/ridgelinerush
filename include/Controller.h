c41a1e0c0840b2dadf5ee7f52b8c1bac784dd5f4
moses alexander <esotericgravity@gmail.com>
2021-04-13 23:04:29 -0400

    Update Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -18,11 +18,28 @@ public:
 
     // mutators //
     void update();
+    void setMvmtUpKey(sf::Keyboard::Key k);
+    void setMvmtLeftKey(sf::Keyboard::Key k);
+    void setMvmtRightKey(sf::Keyboard::Key k);
+    void setAttackKey(sf::Keyboard::Key k);
+    void setPauseKey(sf::Keyboard::Key k);
+
+    // inspectors //
+    sf::Keyboard::Key getMvmtUpKey();
+    sf::Keyboard::Key getMvmtLeftKey();
+    sf::Keyboard::Key getMvmtRightKey();
+    sf::Keyboard::Key getAttackKey();
+    sf::Keyboard::Key getPauseKey();
 
 private:
     // members //
     Hero *playerHero;
     std::string texture;
+    sf::Keyboard::Key mvmt_up;
+    sf::Keyboard::Key mvmt_left;
+    sf::Keyboard::Key mvmt_right;
+    sf::Keyboard::Key attack;
+    sf::Keyboard::Key pause;
 };
 
 #endif // RIDGELINERUSH_CONTROLLER_H
