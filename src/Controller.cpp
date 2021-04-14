b059e24b792ca79c54f1055ea8db20aa476dff24
moses alexander <esotericgravity@gmail.com>
2021-04-13 23:06:01 -0400

    Update Controller.cpp

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -16,21 +16,40 @@ Controller::Controller(Hero *hero) {
 }
 
 void Controller::update(){
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    if (sf::Keyboard::isKeyPressed(mvmt_up))
     {
         (*playerHero).jump();
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    if (sf::Keyboard::isKeyPressed(mvmt_left))
     {
         (*playerHero).driftLeft();
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    if (sf::Keyboard::isKeyPressed(mvmt_right))
     {
         (*playerHero).driftRight();
     }
 }
 
+sf::Keyboard::Key Controller::getMvmtUpKey() { return mvmt_up; }
+
+sf::Keyboard::Key Controller::getMvmtLeftKey() { return mvmt_left; }
+
+sf::Keyboard::Key Controller::getMvmtRightKey() { return mvmt_right; }
+
+sf::Keyboard::Key Controller::getAttackKey() { return attack; }
+
+sf::Keyboard::Key Controller::getPauseKey() { return pause; }
+
+void Controller::setMvmtUpKey(sf::Keyboard::Key k) { mvmt_up = k; }
+
+void Controller::setMvmtLeftKey(sf::Keyboard::Key k) { mvmt_left = k; }
+
+void Controller::setMvmtRightKey(sf::Keyboard::Key k) { mvmt_right = k; }
+
+void Controller::setAttackKey(sf::Keyboard::Key k) { attack = k; }
+
+void Controller::setPauseKey(sf::Keyboard::Key k) { pause = k; }
 
 
