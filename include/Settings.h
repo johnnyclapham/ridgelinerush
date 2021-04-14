7e0d254707e8bd11c5846b20868abe843c268d2b
moses alexander <esotericgravity@gmail.com>
2021-04-13 23:04:50 -0400

    Update Settings.h

--- include/Settings.h
+++ include/Settings.h
@@ -1,5 +1,5 @@
-#ifndef CSCI437_SETTINGS_H
-#define CSCI437_SETTINGS_H
+#ifndef RIDGELINERUSH_SETTINGS_H
+#define RIDGELINERUSH_SETTINGS_H
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -14,9 +14,7 @@ class Settings {
     ScreenSetting::ScreenType screentype;
     sf::Keyboard::Key mvmt_up;
     sf::Keyboard::Key mvmt_left;
-    sf::Keyboard::Key mvmt_down;
     sf::Keyboard::Key mvmt_right;
-    sf::Keyboard::Key jump;
     sf::Keyboard::Key attack;
     sf::Keyboard::Key pause;
 
@@ -34,16 +32,12 @@ class Settings {
     void setScreenType(ScreenSetting::ScreenType st);
     sf::Keyboard::Key getMvmtUpKey();
     sf::Keyboard::Key getMvmtLeftKey();
-    sf::Keyboard::Key getMvmtDownKey();
     sf::Keyboard::Key getMvmtRightKey();
-    sf::Keyboard::Key getJumpKey();
     sf::Keyboard::Key getAttackKey();
     sf::Keyboard::Key getPauseKey();
     void setMvmtUpKey(sf::Keyboard::Key k);
     void setMvmtLeftKey(sf::Keyboard::Key k);
-    void setMvmtDownKey(sf::Keyboard::Key k);
     void setMvmtRightKey(sf::Keyboard::Key k);
-    void setJumpKey(sf::Keyboard::Key k);
     void setAttackKey(sf::Keyboard::Key k);
     void setPauseKey(sf::Keyboard::Key k);
     std::string keyToStr(sf::Keyboard::Key k);
