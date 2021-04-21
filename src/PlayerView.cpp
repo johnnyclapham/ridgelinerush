8a3addbce52c0ad9b9e8effbefb46466b9a5eb15
moses alexander <esotericgravity@gmail.com>
2021-04-20 20:13:53 -0400

    Update PlayerView.cpp

--- src/PlayerView.cpp
+++ src/PlayerView.cpp
@@ -5,6 +5,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h> 
 #include "PlayerView.h"
 #include "Terrain.h"
 #include "Tile.h"
@@ -29,6 +31,8 @@ void PlayerView::updateView(float time) {
     drawHero();
     drawDragon();
     drawProjectiles();
+    reset += (time/GAME_TIME_FACTOR);
+    drawPowerups();
 }
 
 void PlayerView::drawTerrain() {
@@ -140,5 +144,26 @@ void PlayerView::drawProjectiles() {
     for (int i = 0; i < dragon.projectileList.size(); i++) {
         sf::Vector2f position = dragon.projectileList.at(i).getPosition();
         fireballSprite.draw(position, window);
+    }
+}
+
+void PlayerView::drawPowerups() {
+    Hero hero = this->logic->getHero();
+    if (reset > 1000000) {
+        srand(time(NULL));
+        int x = 0; // rand() % 100
+        int y = 0; // rand() % 5
+        if (x < 5) {
+            sf::RectangleShape toDraw = sf::RectangleShape(sf::Vector2f(10, 10));
+            if (y == 0) { toDraw.setFillColor(sf::Color::Green); hero.setPowerupOnScreen(1); }
+            if (y == 1) { toDraw.setFillColor(sf::Color::Blue); hero.setPowerupOnScreen(2); }
+            if (y == 2) { toDraw.setFillColor(sf::Color::Yellow); hero.setPowerupOnScreen(3); }
+            if (y == 3) { toDraw.setFillColor(sf::Color::Red); hero.setPowerupOnScreen(4); }
+            int z1 = hero.getPosition().x;
+            int z2 = hero.getPosition().y;
+            toDraw.setPosition(z1 + 100, z2 + 100);
+            window->draw(toDraw);
+        }
+        reset = 0;
     }
 }
