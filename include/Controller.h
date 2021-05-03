e94f54f5153adf3a514f1cfcab2c99d63431fffd
moses alexander <esotericgravity@gmail.com>
2021-05-02 22:09:49 -0400

    Update Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -23,14 +23,14 @@ public:
     void setMvmtLeftKey(sf::Keyboard::Key k);
     void setMvmtRightKey(sf::Keyboard::Key k);
     void setAttackKey(sf::Keyboard::Key k);
-    void setPauseKey(sf::Keyboard::Key k);
+    void setQuitKey(sf::Keyboard::Key k);
 
     // inspectors //
     sf::Keyboard::Key getMvmtUpKey();
     sf::Keyboard::Key getMvmtLeftKey();
     sf::Keyboard::Key getMvmtRightKey();
     sf::Keyboard::Key getAttackKey();
-    sf::Keyboard::Key getPauseKey();
+    sf::Keyboard::Key getQuitKey();
 
 private:
     // members //
@@ -41,7 +41,7 @@ private:
     sf::Keyboard::Key mvmt_left;
     sf::Keyboard::Key mvmt_right;
     sf::Keyboard::Key attack;
-    sf::Keyboard::Key pause;
+    sf::Keyboard::Key quit;
 };
 
 #endif // RIDGELINERUSH_CONTROLLER_H
