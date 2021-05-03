541989ca98db17bf8c819bc12cd74f8a3cdc4546
moses alexander <esotericgravity@gmail.com>
2021-05-02 22:06:01 -0400

    Update Logic.cpp

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -18,7 +18,6 @@ Logic::Logic() {
     controller = Controller(&hero, &launcher);
     hero = Hero();
     dragon = Dragon(&hero, &terrain);
-    dragonAI = DragonAI(&dragon, &hero);
     settings = Settings();
     background = Background();
 
@@ -26,13 +25,13 @@ Logic::Logic() {
     controller.setMvmtLeftKey(settings.getMvmtLeftKey());
     controller.setMvmtRightKey(settings.getMvmtRightKey());
     controller.setAttackKey(settings.getAttackKey());
-    controller.setPauseKey(settings.getPauseKey());
+    controller.setQuitKey(settings.getQuitKey());
 
     // initialize launchers
     sf::Vector2f heroPosition = hero.getPosition();
-    // sf::Vector2f dragonPosition = dragon.getPosition();
+    sf::Vector2f dragonPosition = dragon.getPosition();
     launcher = Launcher(heroPosition.x, heroPosition.y, 20, 10);
-    // dragonLauncher = Launcher(dragonPosition.x, dragonPosition.y, 50, 50);
+    dragonLauncher = Launcher(dragonPosition.x, dragonPosition.y, 50, 50);
 }
 
 void Logic::update(float time) {
@@ -40,7 +39,6 @@ void Logic::update(float time) {
     hero.update(.5*time, terrain);
     terrain.move(.5*time, .5*time);
     dragon.update(.5*time);
-    dragonAI.update(.5*time);
     settings.read();
     launcher.updateHero(.5*time, &terrain, &dragon, &hero);
     dragonLauncher.updateDragon(.5*time, &terrain, &dragon, &hero);
